70a277c6ca6822c09ae5b0233226ee26990ef61e
abin c joseph <abincjoseph77@gmail.com>
2025-06-20 12:51:05 +0530

    requirements.txt

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #render
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CRM.urls'
